e8c07516895f5d2ac897845cd50d71636341c2bc
divamgupta <sdfasdfasdfasdf>
2017-04-16 23:25:08 +0530

    final cpp dataset gen done

--- examples/generate_dataset.cc
+++ examples/generate_dataset.cc
@@ -17,93 +17,87 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-  while(1)
-  {
+
+  try {
+    
+    string file_name ; //  = "/home/iiitd/libhand/hand_model/scene_spec.yml";
+
+    vector<int> bend ; 
+    vector<int> side ; 
+    vector<int> twist ; 
+    string outFName; // = "test.jpg" ; 
+    int theta = 360;
+    int phi = 360 ; 
+    int tilt = 360 ;
+    int dist = 100;
+
+    cout << "enter the scene filename " << endl;
+    getline(cin, file_name );
+
+    cout << "enter the output filename " << endl;
+    getline(cin, outFName );
+    cout << "enter the camera theta (360 )" << endl;
+    cin >> theta ;
+    cout << "enter the camera phi (360 )" << endl;
+    cin >> phi ;
+    cout << "enter the camera tilt (360 ) " << endl;
+    cin >> tilt ;
+    cout << "enter the camera dist ( 100 )" << endl;
+    cin >> dist ;
+
+    for( int i=0; i<18 ; i++ )
+    {
+        int b , s , t ;
+        cout << "enter the joint " << i << " bend (360)" << endl;
+        cin >> b;
+        cout << "enter the joint " << i << " side (360)" << endl;
+        cin >> s;
+        cout << "enter the joint " << i << " tilt (360)" << endl;
+        cin >> t ;
+        bend.push_back( b );
+        side.push_back( s );
+        twist.push_back( t );
+    }
+
+   
+
+    
+
+    HandRenderer hand_renderer;
+
+    hand_renderer.Setup();
+    SceneSpec scene_spec(file_name);
+    hand_renderer.LoadScene(scene_spec);
+    cv::Mat pic = hand_renderer.pixel_buffer_cv();
+    FullHandPose hand_pose(scene_spec.num_bones());
 
 
-      try {
-        
-        string file_name ; //  = "/home/iiitd/libhand/hand_model/scene_spec.yml";
+    for( int i=0; i<18 ; i++ )
+    {
+        hand_pose.bend(i) = 3.14159 * bend[i] / (180.00  );
+        hand_pose.side(i) = 3.14159 * side[i] / (180.00  ) ;
+        hand_pose.twist(i) = 3.14159 * twist[i] /  (180.00 )  ;
+    }
 
-        vector<int> bend ; 
-        vector<int> side ; 
-        vector<int> twist ; 
-        string outFName; // = "test.jpg" ; 
-        int theta = 360;
-        int phi = 360 ; 
-        int tilt = 360 ;
-        int dist = 100;
+    HandCameraSpec camera_spec_;
+    camera_spec_ = HandCameraSpec(hand_renderer.initial_cam_distance());
+    camera_spec_.theta = 3.14159*theta/180.0 ;
+    camera_spec_.phi =  3.14159*phi/180.0 ;
+    camera_spec_.tilt =  3.14159*tilt/180.0 ;
+    camera_spec_.r =  dist/10.0 ;
 
-        cout << "enter the scene filename or exit " << endl;
-        getline(cin, file_name );
+    hand_pose.SetRotMatrix(camera_spec_);
+    hand_renderer.set_camera_spec(camera_spec_); 
+    hand_renderer.SetHandPose(hand_pose);
+    hand_renderer.RenderHand();
 
-        if( file_name == "exit")
-            break;
+    imwrite( outFName, pic );
+    cout << "written : " << outFName << endl;
 
-        cout << "enter the output filename " << endl;
-        getline(cin, outFName );
-        cout << "enter the camera theta (360 )" << endl;
-        cin >> theta ;
-        cout << "enter the camera phi (360 )" << endl;
-        cin >> phi ;
-        cout << "enter the camera tilt (360 ) " << endl;
-        cin >> tilt ;
-        cout << "enter the camera dist ( 100 )" << endl;
-        cin >> dist ;
-
-        for( int i=0; i<18 ; i++ )
-        {
-            int b , s , t ;
-            cout << "enter the joint " << i << " bend (360)" << endl;
-            cin >> b;
-            cout << "enter the joint " << i << " side (360)" << endl;
-            cin >> s;
-            cout << "enter the joint " << i << " tilt (360)" << endl;
-            cin >> t ;
-            bend.push_back( b );
-            side.push_back( s );
-            twist.push_back( t );
-        }
-
-       
-
-        
-
-        HandRenderer hand_renderer;
-
-        hand_renderer.Setup();
-        SceneSpec scene_spec(file_name);
-        hand_renderer.LoadScene(scene_spec);
-        cv::Mat pic = hand_renderer.pixel_buffer_cv();
-        FullHandPose hand_pose(scene_spec.num_bones());
-
-
-        for( int i=0; i<18 ; i++ )
-        {
-            hand_pose.bend(i) = 3.14159 * bend[i] / (180.00  );
-            hand_pose.side(i) = 3.14159 * side[i] / (180.00  ) ;
-            hand_pose.twist(i) = 3.14159 * twist[i] /  (180.00 )  ;
-        }
-
-        HandCameraSpec camera_spec_;
-        camera_spec_ = HandCameraSpec(hand_renderer.initial_cam_distance());
-        camera_spec_.theta = 3.14159*theta/180.0 ;
-        camera_spec_.phi =  3.14159*phi/180.0 ;
-        camera_spec_.tilt =  3.14159*tilt/180.0 ;
-        camera_spec_.r =  dist/10.0 ;
-
-        hand_pose.SetRotMatrix(camera_spec_);
-        hand_renderer.set_camera_spec(camera_spec_); 
-        hand_renderer.SetHandPose(hand_pose);
-        hand_renderer.RenderHand();
-
-        imwrite( outFName, pic );
-        cout << "written : " << outFName << endl;
-
-      } catch (const std::exception &e) {
-        cerr << "Exception: " << e.what() << endl;
-      }
+  } catch (const std::exception &e) {
+    cerr << "Exception: " << e.what() << endl;
   }
+
 
   return 0;
 }
